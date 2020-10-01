95ad31ae9a5ee70248bee169174bd3428877273a
Cem Cebi <cemberkecebi@gmail.com>
2020-10-01 17:43:34 +0300

    first commit

---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <thead>
+      <tr>
+        <th>Employee Name</th>
+        <th>Employee Email</th>
+        <th>Actions</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="employee in employees" :key="employee.id">
+        <td>{{ employee.name }}</td>
+        <td>{{ employee.email }}</td>
+        <td>
+          <button @click="editMode(employee.id)">Edit</button>
+          <button @click="$emit('delete:employee',employee.id)">Delete</button>
+        </td>
+      </tr>
+      </tbody>
+      <p v-if="employees.length < 1" class="empty-table">No employees!!!</p>
+    </table>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'employee-table',
+  props: {
+    employees: Array,
+  },
+  data() {
+    return {
+      editing: null,
+    }
+  },
+  methods: {
+    editMode(id) {
+      this.editing = id;
+    },
+    editEmployee(employee) {
+      if (employee.name === '' || employee.email === '') return
+      this.emit('edit:employee',employee.id, employee)
+      this.editing = null
+    }
+  }
+
+}
+</script>
+
+
+<style scoped>
+button {
+  margin-right: 0.5rem;
+}
+
+.empty-table {
+  color: #d33c40;
+  font-weight: bolder;
+}
+</style>
