2368ed86b5aec6c9b2202a309b895cca8cfb36ac
Cem Cebi <cemberkecebi@gmail.com>
2020-10-03 23:12:21 +0300

    editing feature and rest api calls added

--- src/components/EmployeeTable.vue
+++ src/components/EmployeeTable.vue
@@ -10,12 +10,26 @@
       </thead>
       <tbody>
       <tr v-for="employee in employees" :key="employee.id">
-        <td>{{ employee.name }}</td>
-        <td>{{ employee.email }}</td>
-        <td>
-          <button @click="editMode(employee.id)">Edit</button>
+        <td v-if="editing === employee.id">
+          <input type="text" v-model="employee.name">
+        </td>
+        <td v-else>{{ employee.name }}</td>
+
+        <td v-if="editing === employee.id">
+          <input type="text" v-model="employee.email">
+        </td>
+        <td v-else>{{ employee.email }}</td>
+
+        <td v-if="editing === employee.id">
+          <button @click="editEmployee(employee)">Save</button>
+          <button class="muted-button" @click="cancelEdit(employee)">Cancel</button>
+        </td>
+
+        <td v-else>
+          <button @click="editMode(employee)">Edit</button>
           <button @click="$emit('delete:employee',employee.id)">Delete</button>
         </td>
+
       </tr>
       </tbody>
       <p v-if="employees.length < 1" class="empty-table">No employees!!!</p>
@@ -36,12 +50,18 @@ export default {
     }
   },
   methods: {
-    editMode(id) {
-      this.editing = id;
+    editMode(employee) {
+      this.cachedEmployee = Object.assign({}, employee)
+      this.editing = employee.id
+    },
+    cancelEdit(employee) {
+      Object.assign(employee, this.cachedEmployee)
+      this.editing = null;
     },
     editEmployee(employee) {
+      console.log("editig")
       if (employee.name === '' || employee.email === '') return
-      this.emit('edit:employee',employee.id, employee)
+      this.$emit('edit:employee', employee.id, employee)
       this.editing = null
     }
   }
